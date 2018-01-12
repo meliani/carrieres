0c1bb661372ac8a87ec8f88b1c9efa4e62aa83f3
MELIANI <melianiplus@gmail.com>
2018-01-12 11:46:25 +0000

    Change name of class to CreatePeoplesTable.

--- database/migrations/2017_11_30_222439_create_people_table.php
+++ database/migrations/2017_11_30_222439_create_people_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePeopleTable extends Migration
+class CreatePeoplesTable extends Migration
 {
     /**
      * Run the migrations.
