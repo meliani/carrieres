3d437d7c4a5529fde3869a1d70ff470ef117a005
MELIANI <melianiplus@gmail.com>
2018-11-09 11:49:16 +0000

    Actualisation des documents pour l'année 18-19

--- resources/views/edocs/index.blade.php
+++ resources/views/edocs/index.blade.php
@@ -6,10 +6,28 @@
   <ul class="collection">
     <li class="collection-item avatar">
       <i class="material-icons circle">folder</i>
-      <span class="title">Recommendations</span>
+      <span class="title">La lettre de recommendations</span>
       <p>
+        <a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/Lettre%20Recommandation%20-%20INPT.pdf">
+          Lettre de recommandations du cycle INE à l'INPT.
+        </a>
+      </p>
+      <a href="#" class="secondary-content"><i class="material-icons">grade</i></a>
+      <p>
+        <a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/Lettre%20Recommandation%20-%20Mobilit%C3%A9.pdf">
+          Lettre de recommandations du cycle INE en mobilité international.
+        </a>
+      </p>
+      <a href="#" class="secondary-content"><i class="material-icons">grade</i></a>
+    </li>
+
+    <li class="collection-item avatar">
+      <i class="material-icons circle">folder</i>
+      <span class="title">Le formulaire d'encadrement</span>
+      <p>
+        Les documents au titre de l'année 2019 sont en cours de préparation pour des raisons d'actualisation du réglement.
         <a href="#">
-          Télécharger votre Lettre de Recommandations
+            
         </a>
       </p>
       <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
@@ -17,24 +35,16 @@
 
     <li class="collection-item avatar">
       <i class="material-icons circle">folder</i>
-      <span class="title">Formulaire d'encadrement</span>
+      <span class="title">La Convention de Stage</span>
       <p>
-        <a href="#">
-          Télécharger le formulaire d'encadrement
-        </a>
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-
-    <li class="collection-item avatar">
-      <i class="material-icons circle">folder</i>
-      <span class="title">Convention</span>
-      <p>
-          <ul>          
+          <ul>     
+            Les documents au titre de l'année 2019 sont en cours de préparation pour des raisons d'actualisation du réglement. 
+            <!--    
             <li><a href="#">Convention de stages type Maroc et autres pays</a></li>
             <li><a href="#">Convention de stages type France</a></li>
             <li><a href="#">Convention de stages type Télécom ParisTech</a></li>
             <li><a href="#">Convention de stages type Télécom SudParis</a></li>
+            -->
           </ul>
       </p>
       <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
