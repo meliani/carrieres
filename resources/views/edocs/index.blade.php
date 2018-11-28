f428c13ac721f07b147d134a3a73fb7cb0fc5746
MELIANI <melianiplus@gmail.com>
2018-11-28 10:14:09 +0000

    mise a jour des documents

--- resources/views/edocs/index.blade.php
+++ resources/views/edocs/index.blade.php
@@ -20,7 +20,7 @@
       </p>
       <a href="#" class="secondary-content"><i class="material-icons">grade</i></a>
     </li>
-
+<!--
     <li class="collection-item avatar">
       <i class="material-icons circle">folder</i>
       <span class="title">Le formulaire d'encadrement</span>
@@ -32,19 +32,20 @@
       </p>
       <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
     </li>
-
+  -->
     <li class="collection-item avatar">
       <i class="material-icons circle">folder</i>
       <span class="title">La Convention de Stage</span>
       <p>
           <ul>     
-            Les documents au titre de l'année 2019 sont en cours de préparation pour des raisons d'actualisation du réglement. 
-            <!--    
-            <li><a href="#">Convention de stages type Maroc et autres pays</a></li>
-            <li><a href="#">Convention de stages type France</a></li>
-            <li><a href="#">Convention de stages type Télécom ParisTech</a></li>
-            <li><a href="#">Convention de stages type Télécom SudParis</a></li>
-            -->
+            <!-- Les documents au titre de l'année 2019 sont en cours de préparation pour des raisons d'actualisation du réglement. 
+                -->
+            <li><a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/1 - Conv type st au Maroc et autres pays que la France">Convention de stages type Maroc et autres pays</a></li>
+            <li><a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/2 - Conv type st en France.docx">Convention de stages type France</a></li>
+            <li><a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/3 - Conv type TELECOM SudParis.doc">Convention de stages type Télécom SudParis</a></li>
+            <li><a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/4 - Conv type Télécom ParisTech.doc">Convention de stages type Télécom ParisTech</a></li>
+            <li><a href="https://carrieres.inpt.ac.ma/Files/1819/Docs/5 - Conv type Cukurova.doc">Convention de stages type Cukurova</a></li>
+
           </ul>
       </p>
       <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
