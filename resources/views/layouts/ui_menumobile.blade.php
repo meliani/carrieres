3a9427550c73c65af70cc14d5e4ef05fdbd4d812
MELIANI <melianiplus@gmail.com>
2018-12-27 00:47:50 +0000

    added role intead of can

--- resources/views/layouts/ui_menumobile.blade.php
+++ resources/views/layouts/ui_menumobile.blade.php
@@ -1,20 +1,20 @@
 @role('Admin')
 <li><a href="{{ route('admin.offresDeStages.index') }}">Adminisration</a></li>
 @endrole
-@can('see advisors')
+@role('Examinateur')
 <li><a href="{{ route('mesEncadrements.index') }}">Mes encadrements</a></li>
 <li class="divider"></li>
-@endcan
+@endrole
 @role('Etudiant')
 <li class="{{ Request::is('monStage.index') ? 'active' : '' }}"><a class="blue-grey-text text-darken-1" href="{{ route('monStage.index') }}">Voir les offres</a></li>
 <li class="{{ Request::is('monStage.guide') ? 'active' : '' }}"><a class="blue-grey-text text-darken-1" href="{{ route('edocs.index') }}">Mes documents</a></li>
 <li class="{{ Request::is('internship.create') ? 'active' : '' }}"><a class="blue-grey-text text-darken-1" href="{{ route('internship.create') }}">Declarer mon stage</a></li>
-
 @endrole
 @guest
   <li class="{{ Request::is('login*') ? 'active' : '' }}"><a class="blue-grey-text text-darken-1" href="{{ route('login') }}">Login</a></li>
-@else
 <li class="divider"></li>
+@else
+
 <li><a href="{{ route('logout') }}"
     onclick="event.preventDefault();
             document.getElementById('logout-form').submit();">
