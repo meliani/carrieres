025d9258ab7b0cdce4787f208b2a4078eaee8be0
MELIANI <melianiplus@gmail.com>
2018-12-28 11:21:00 +0000

    middlware

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
     Route::middleware(['isAdmin'])->group(function () {
+        
     Route::get('admin/applications', ['as'=> 'admin.applications.index', 'uses' => 'Admin\applicationsController@index']);
 
     Route::get('admin/offresDeStages', ['as'=> 'admin.offresDeStages.index', 'uses' => 'Admin\offresDeStagesController@index']);
