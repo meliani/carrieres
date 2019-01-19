dbcfdd8f055a0ecd13c342586587b7572b2762d8
MELIANI <melianiplus@gmail.com>
2019-01-19 23:04:16 +0100

    Snappy pdf installing and making the first view

--- config/snappy.php
+++ config/snappy.php
@@ -5,31 +5,20 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => env('SNAPPY_PDF_BINARY','wkhtmltopdf'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
+        'binary'  => env('SNAPPY_IMAGE_BINARY','wkhtmltoimage'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
 
-
+// binary ='/usr/local/bin/wkhtmltopdf'
 );
-/*
-return array(
-    'pdf' => array(
-        'enabled' => true,
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
-        'options' => array(),
-    ),
-    'image' => array(
-        'enabled' => true,
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"',
-        'options' => array(),
-    ),
-);*/
+
+
---
+++ resources/views/edocs/pdf.recommendation.blade.php
@@ -0,0 +1,20 @@
+<p class="left">Rabat, le 25 Octobre 2018</p>
+<p>Objet : Lettre de recommandation</p>
+<p>Madame, Monsieur,</p>
+<p>Nous vous remercions de bien vouloir accorder un Stage de Projet de Fin d’Etudes
+(PFE) à M. {{ student->name }}, élève ingénieur de 3ème année à l’INPT de Rabat. En
+accueillant notre élève stagiaire au sein de votre entreprise, vous auriez contribué à
+une meilleure préparation pour son insertion dans la vie professionnelle.</p>
+<p>Les stages de PFE intégrés à la structure pédagogique du cycle des Ingénieurs
+d’Etat (INE), permettent à l’élève ingénieur d’effectuer, en situation réelle, une
+recherche personnelle approfondie sur le thème du projet proposé. En plus, grâce à
+vos appréciations des capacités et des compétences de notre élève, nous pourrions
+améliorer notre action pédagogique en rapprochant notre formation aux exigences
+du marché de travail.</p>
+<p>A l’issue de ce stage, l’élève ingénieur est tenu d’élaborer un rapport dans lequel il
+présentera les résultats de son travail. Une copie dudit rapport sera remise à l’INPT
+et une autre à votre entreprise. L’élève devra également soutenir son sujet de PFE
+devant un jury composé d’enseignants de l’INPT et d’un ou plusieurs représentant(s)
+de votre honorable entreprise que vous voudriez bien désigner.
+En vous remerciant de votre coopération, nous vous prions d’agréer, Madame,
+Monsieur, l’expression de nos salutations les meilleures.</p>
