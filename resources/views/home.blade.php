86036c133ada42f27f615dc64949a100d67cdb47
Meliani <melianiplus@gmail.com>
2019-03-07 13:34:55 +0100

    impr : some changes on the home page for more conveinience

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,39 +9,31 @@
         </div>
     @endif
 </div>
-<div class="container">
+<div class="container home">
     <div class="row">
-        <div class="col s12 m6">
-
-            
-            <div class="card">
-                <div class="card-content">
-                    Bienvenue. Cliquez sur le menu au-dessus pour explorer les fonctionnalités de la platforme carrières.
-                </div>
+    <div class="col s12 m4 l4">
+        <h6>Bienvenue</h6>
+        <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+            <img src="https://image.flaticon.com/icons/svg/1256/1256649.svg" width="64" height="64">
+            <p>Cliquez sur le menu au-dessus pour explorer les fonctionnalités de la platforme carrières.</p>
             </div>
-        </div>       
-        <div class="col s12 m6">
-            <div class="card">
-                <div class="card-content">
-                    Welcome. Click on the menu above to explore our eservices.
+            <div class="card-action">
+            <a href="#">Voir mon profile</a>
+            </div>
+        </div>
+        </div>
+        <div class="col s12 m4 l4">
+        <h6>Welcome</h6>
+            <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                <img src="https://image.flaticon.com/icons/svg/1256/1256649.svg" width="64" height="64">
+                <p>Click on the menu above to explore our eservices.</p>
+                </div>
+                <div class="card-action">
+                <a href="#">View my profile</a>
                 </div>
             </div>
         </div>
-<!--
-        <div class="col s12 m6 l3">
-            <div class="card">
-                <div class="card-content  green white-text">
-                    <p class="card-stats-title"><i class="mdi-social-group-add"></i> New Clients</p>
-                    <h4 class="card-stats-number">566</h4>
-                    <p class="card-stats-compare"><i class="mdi-hardware-keyboard-arrow-up"></i> 15% <span class="green-text text-lighten-5">from yesterday</span>
-                    </p>
-                </div>
-                <div class="card-action  green darken-2">
-                    <div id="clients-bar"><canvas width="290" height="25" style="display: inline-block; width: 290px; height: 25px; vertical-align: top;"></canvas></div>
-                </div>
-            </div>
-        </div>
-    </div>
--->
 </div>
 @endsection
