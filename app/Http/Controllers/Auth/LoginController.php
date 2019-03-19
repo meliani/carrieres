63c5210cc4f7c9d6a15f212b26a7512ae5c1e004
Meliani <melianiplus@gmail.com>
2019-03-19 11:20:22 +0100

    fix : some edits concerning People

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\School\Profile\People;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
+
+    public function redirectTo(){
+        $person = People::find(auth()->user()->id);
+        if(!$person->activate()){
+            return '/Activation';
+        }else{
+            return '/home';
+        }
+    }
 
     /**
      * Create a new controller instance.
--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,6 +1,6 @@
 <div class="col s12 m4 l4">
     <h6>Bienvenue</h6>
-    <div class="card blue-grey darken-1">
+    <div class="card blue-grey lighten-1">
         <div class="card-content white-text">
             <img src="https://image.flaticon.com/icons/svg/138/138701.svg" width="64" height="64">
             <p>
@@ -8,13 +8,28 @@
             </p>
         </div>
         <div class="card-action">
-            <a href="{{ route('people.activate') }}">Valider mon profile</a>
+            <a href="#"></a>
         </div>
     </div>
 </div>
 
 <div class="col s12 m4 l4">
-    <h6>Stages d'été</h6>
+        <h6>Mon profil</h6>
+        <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+                <img src="https://image.flaticon.com/icons/svg/138/138701.svg" width="64" height="64">
+                <p>
+                    Veuillez valider votre profil en cliquant sur le bouton ci dessous.
+                </p>
+            </div>
+            <div class="card-action">
+                <a href="{{ route('people.activate') }}">Valider mon profil</a>
+            </div>
+        </div>
+    </div>
+
+<div class="col s12 m4 l4">
+    <h6>Opportunités de stage</h6>
     <div class="card blue-grey darken-1">
         <div class="card-content white-text">
         <img src="https://image.flaticon.com/icons/svg/1571/1571309.svg" width="64" height="64">
