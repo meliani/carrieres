c1e0e58687e0a2fe3efc9816205bc087e77daeff
Meliani <melianiplus@gmail.com>
2019-03-26 17:56:10 +0100

    fixed some texts

--- resources/views/space/internship/advising/list.blade.php
+++ resources/views/space/internship/advising/list.blade.php
@@ -2,7 +2,7 @@
   <table class="table highlight scale-transition scale-in">
     <thead>
       <tr>
-        <th width="5%">id</th>
+        <th width="5%">Id</th>
         <th width="13%">Nom et prénom</th>
         <th width="15%">Entreprise</th>
           <th width="25%">Titre du PFE</th>
