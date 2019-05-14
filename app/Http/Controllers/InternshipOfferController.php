1b8b72350b5e73c65e32dd98059d90dddea15198
Meliani <melianiplus@gmail.com>
2019-05-14 01:00:03 +0100

    Add model and controllers for Offer,Student

---
+++ app/Http/Controllers/InternshipOfferController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\School\Internship\internshipOffer;
+use Illuminate\Http\Request;
+
+class InternshipOfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\School\Internship\internshipOffer  $internshipOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(internshipOffer $internshipOffer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\School\Internship\internshipOffer  $internshipOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(internshipOffer $internshipOffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\School\Internship\internshipOffer  $internshipOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, internshipOffer $internshipOffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\School\Internship\internshipOffer  $internshipOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(internshipOffer $internshipOffer)
+    {
+        //
+    }
+}
--- app/Models/School/Profile/Student.php
+++ app/Models/School/Profile/Student.php
@@ -2,13 +2,9 @@
 
 namespace App\Models\School\Profile;
 
-use App\User;
-use App\Models\School\Profile\People;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\School\Internship\Internship;
 
-class Student extends People
+class Student extends Model
 {
-
-
+    //
 }
