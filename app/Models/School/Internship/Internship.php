9d49920e95fab7ddddc24041112545d934f57c35
Meliani <melianiplus@gmail.com>
2019-05-15 03:13:20 +0100

    fix : bug for dates because the field is not a timestamp

--- app/Models/School/Internship/Internship.php
+++ app/Models/School/Internship/Internship.php
@@ -20,9 +20,7 @@ class Internship extends Model
     protected $dates = [
         'created_at',
         'updated_at',
-        'deleted_at',
-        'date_debut',
-        'date_fin'
+        'deleted_at'
     ];
     
     public $fillable = [
