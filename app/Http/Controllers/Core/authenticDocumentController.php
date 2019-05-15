bbdf2dbee9e7716ee16a35ea5fededabe610c93d
Meliani <melianiplus@gmail.com>
2019-05-15 01:26:20 +0100

    is admin middlware

--- app/Http/Controllers/Core/authenticDocumentController.php
+++ app/Http/Controllers/Core/authenticDocumentController.php
@@ -9,7 +9,7 @@ class authenticDocumentController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth','Admin']);
+        $this->middleware(['auth','isAdmin']);
     }
     /**
      * Display a listing of the resource.
