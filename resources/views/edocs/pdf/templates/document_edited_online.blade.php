9f1e5d281831835dd66fbb937d7c4aaa8b21bfc8
Meliani <melianiplus@gmail.com>
2019-05-15 02:41:44 +0100

    fix mention

--- resources/views/edocs/pdf/templates/document_edited_online.blade.php
+++ resources/views/edocs/pdf/templates/document_edited_online.blade.php
@@ -1,4 +1,5 @@
-<p align="right" style="margin-bottom: 0cm; line-height: 100%;float:left;"><font size="1" style="font-size: 8pt">Document
-&eacute;dit&eacute; en ligne via la plateforme INPT Entreprises :  
+<p align="right" style="margin-bottom: 0cm; line-height: 100%;float:left;">
+    <font size="1" style="font-size: 8pt">Document
+g&eacute;n&eacute;r&eacute; automatiquement via la plateforme INPT Entreprises :  
 {{ Carbon\Carbon::now()->isoFormat('dddd Do MMMM YYYY, hh:MM') }}</font>
 </p>
