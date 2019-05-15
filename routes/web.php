4e45f4c516aa22eef8642e0467c3d777365a6a7b
Meliani <melianiplus@gmail.com>
2019-05-15 18:32:23 +0100

    fix nopermissions problem on routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::middleware(['isAdmin'])->group(function () {
+Route::middleware(['auth','isAdmin'])->group(function () {
 
 Route::view('extractions','extractions.index')->name('extractions');
 
