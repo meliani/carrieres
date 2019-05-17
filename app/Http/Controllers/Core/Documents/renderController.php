988c2bd207c6b53f836b9bb169bda6e0d56f705e
Meliani <melianiplus@gmail.com>
2019-05-17 23:26:18 +0100

    Student middlware change from Etudiant

--- app/Http/Controllers/Core/Documents/renderController.php
+++ app/Http/Controllers/Core/Documents/renderController.php
@@ -12,7 +12,7 @@ class renderController extends Controller
     
     public function __construct()
     {
-        $this->middleware(['auth','Etudiant']);
+        $this->middleware(['auth','Student']);
     }
     private $file_path;
     public function convention(){
