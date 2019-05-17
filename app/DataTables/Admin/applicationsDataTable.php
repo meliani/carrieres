99759286aadc0380de0722b98475e27960e6b75c
Meliani <melianiplus@gmail.com>
2019-05-17 22:16:15 +0100

    correct a cat typin error :D (while she walks on the keyboard)

--- app/DataTables/Admin/applicationsDataTable.php
+++ app/DataTables/Admin/applicationsDataTable.php
@@ -2,7 +2,7 @@
 
 namespace App\DataTables\Admin;
 
-use App\Models\Application;
+use App\Models\School\Internship\Application;
 use Yajra\DataTables\Services\DataTable;
 use Yajra\DataTables\EloquentDataTable;
 
