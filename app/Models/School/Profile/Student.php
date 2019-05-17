e2cad6289cb92d1bc064e71edac2034f497c402d
Meliani <melianiplus@gmail.com>
2019-05-17 23:22:32 +0100

    adding some relations

--- app/Models/School/Profile/Student.php
+++ app/Models/School/Profile/Student.php
@@ -4,12 +4,17 @@ namespace App\Models\School\Profile;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\School\Profile\People;
+use App\Models\School\Internship\Internship;
 
 class Student extends People
 {
 
     public function internship()
     {
-        return $this->hasOne(Internship::class,'user_id','user_id');
+        return $this->hasMany(Internship::class,'user_id','user_id');
+    }
+    public function agreement()
+    {
+        return $this->hasOne(InternshipAgreement::class,'user_id','user_id');
     }
 }
