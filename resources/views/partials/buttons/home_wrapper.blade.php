7c1c7d92cd66e971616732d331b0978b5a1519af
Meliani <melianiplus@gmail.com>
2019-05-17 22:23:49 +0100

    adding a home button for call alone

---
+++ resources/views/partials/buttons/home_wrapper.blade.php
@@ -0,0 +1,7 @@
+<ul class="right hide-on-med-and-down">
+        @include('partials.buttons.home')
+<ul>
+@section('page-script')
+        @parent
+        @include('partials.buttons.script')
+@stop
