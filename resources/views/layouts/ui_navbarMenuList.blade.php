2125ca96f61faec1a7b9c49b7929a5a38c910b57
Meliani <melianiplus@gmail.com>
2019-05-18 06:41:29 +0100

    deleting the mes encadremets button

--- resources/views/layouts/ui_navbarMenuList.blade.php
+++ resources/views/layouts/ui_navbarMenuList.blade.php
@@ -7,9 +7,5 @@
   <li><a class='dropdown-trigger blue-grey-text text-darken-1' 
     href="#" data-target ='dropdown1'>{{ Auth::user()->name }}
     <i class="material-icons right">arrow_drop_down</i></a></li>
-  @can('see advisors')
-  <li><a href="{{ route('mesEncadrements.index') }}">
-    <i class="material-icons left">group</i>Mes encadrements</a></li>
-  <li class="divider"></li>
-  @endcan
+
 @endguest
