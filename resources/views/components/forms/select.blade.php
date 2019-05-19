99e85bacf531088dd10b0757da8c7133e1f3aeee
Meliani <melianiplus@gmail.com>
2019-05-19 20:22:08 +0100

    Feat : improvements for component rendering

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -3,19 +3,19 @@ $type=array_get($params,'type','text');
 ?>
 <div class = "input-field 
     col 
-    s{{ array_get($params,'cols',6)*3 }}
+    s{{ array_get($params,'cols',6)*2 }}
     m{{ array_get($params,'cols',6) }}
     l{{ array_get($params,'cols',6)/1.5 }}
     ">
     <i class="material-icons blue-text prefix">{{ array_get($params,'icon') }}</i>
-    {!! Form::label(array_get($params,'name'), array_get($params,'label')) !!}
-    {!! Form::$type(array_get($params,'name'), array_get($params,'value'), 
-    ['class' => array_get($params,'class'),
-     'placeholder' => array_get($params,'placeholder'),
-     'value' => array_get($params,'value'),
-     array_get($params,'required'),
+
+    {!! Form::select(array_get($params,'name'),
+    [
+        null => array_get($params,'null', null)
     ]
-    ) !!}
+    +
+    array_get($params,'data')) !!}
+    {!! Form::label(array_get($params,'name'), array_get($params,'label')) !!}
 
     <span class="helper-text" 
     {{ $errors->has(array_get($params,'name')?'data-error='.
