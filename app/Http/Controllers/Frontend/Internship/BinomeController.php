0c77fb81d2f5ca874fc56f6d08b4ce4857797add
Meliani <melianiplus@gmail.com>
2019-05-20 19:11:05 +0100

    Creating a binome controller

---
+++ app/Http/Controllers/Frontend/Internship/BinomeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Internship;
+
+use App\Http\Controllers\Controller;
+use App\Models\School\Internship;
+use Illuminate\Http\Request;
+
+class BinomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $internship = Internship::find(request('id'));
+        $internship ='';
+        return view('frontend.internships.my_internship.binome.create',compact('internship'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        //dump($request);
+
+        $input = $request->all();
+        //dd($request->user()->id);
+        $internship = new Internship($input);
+        $internship->user()->associate(auth()->user()->id);
+        $internship->save();
+        //Flash::success('Votre déclaration a été bien enregistrée.');
+        return back()
+        ->with('message', 'Votre déclaration a été bien enregistrée.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Internship $internship)
+    {
+        //
+    }
+}
