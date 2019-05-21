ba14d4c99b114051cbd1413aa009a71b60f64b3f
Meliani <melianiplus@gmail.com>
2019-05-21 05:55:41 +0100

    Creating user internship assets

---
+++ app/Models/School/Intenrship/userInternship.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\School\Intenrship;
+
+use Illuminate\Database\Eloquent\Model;
+
+class userInternship extends Model
+{
+    //
+}
---
+++ database/migrations/2019_05_21_024109_create_user_internships_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInternshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_internships', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->integer('internship_id')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_internships');
+    }
+}
