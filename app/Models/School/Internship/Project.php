56387698a8e3a690b136aaadea20861f53d8fd26
Meliani <melianiplus@gmail.com>
2019-05-22 04:34:00 +0100

    Project extends Internship to get only valid internships

--- app/Models/School/Internship/Project.php
+++ app/Models/School/Internship/Project.php
@@ -7,5 +7,18 @@ use App\Models\School\Internship;
 
 class Project extends Internship
 {
-    //
+
+
+    protected $table = 'internships';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(function ($query) {
+            $query->where('status', '1');
+        });
+    }
+
+
 }
