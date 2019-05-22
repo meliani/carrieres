7880f973611b35b54c1f63061af186d10dfe220e
Meliani <melianiplus@gmail.com>
2019-05-22 06:37:17 +0100

    link to internships page

--- resources/views/backend/partials/cards/internships.blade.php
+++ resources/views/backend/partials/cards/internships.blade.php
@@ -8,7 +8,7 @@
         <span class="card-title activator grey-text text-darken-4">
             {{ $count['internships'] }} Stages concrétisés
             <i class="material-icons right">more_vert</i></span>
-        <p><a href={{ url('') }}>Voir la liste</a></p>
+        <p><a href={{ url('-/internships') }}>Voir la liste</a></p>
         </div>
         <div class="card-reveal">
         <span class="card-title grey-text text-darken-4">
