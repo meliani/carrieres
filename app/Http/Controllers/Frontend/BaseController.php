f1d84ee8cf16ebf06b47ae0c1d49cfb13c9f5996
Meliani <melianiplus@gmail.com>
2019-05-22 02:54:35 +0100

    adding construct method with middlwares

--- app/Http/Controllers/Frontend/BaseController.php
+++ app/Http/Controllers/Frontend/BaseController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class BaseController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth','isAdmin','Student','Teacher']);        
+    }
 }
