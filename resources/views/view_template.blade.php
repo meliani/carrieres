61435e074ba1b7cb0b2ef6c4490f55ed5cd69d32
Meliani <melianiplus@gmail.com>
2019-05-22 03:10:43 +0100

    template update

--- resources/views/view_template.blade.php
+++ resources/views/view_template.blade.php
@@ -1,7 +1,15 @@
-@extends('app')
+@extends('layouts.ui')
 
 @section('title', 'This is my title')
 
+@section('users_buttons')
+    @include(Button::user_buttons())
+@endsection  
+
 @section('content')
 
+@endsection
+
+@section('floating-buttons')
+    @include(Button::page_action_buttons())
 @endsection
