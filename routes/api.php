b0e745a6c3d69e7e882a8c1d86a938cd1a9cd3ff
Meliani <melianiplus@gmail.com>
2019-05-22 06:12:57 +0100

    purification

--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::get('admin/offres_de_stages', 'Admin\offresDeStagesAPIController@index');
-Route::post('admin/offres_de_stages', 'Admin\offresDeStagesAPIController@store');
-Route::get('admin/offres_de_stages/{offres_de_stages}', 'Admin\offresDeStagesAPIController@show');
-Route::put('admin/offres_de_stages/{offres_de_stages}', 'Admin\offresDeStagesAPIController@update');
-Route::patch('admin/offres_de_stages/{offres_de_stages}', 'Admin\offresDeStagesAPIController@update');
-Route::delete('admin/offres_de_stages{offres_de_stages}', 'Admin\offresDeStagesAPIController@destroy');
-
-Route::get('admin/report_submissions', 'Admin\reportSubmissionAPIController@index');
-Route::post('admin/report_submissions', 'Admin\reportSubmissionAPIController@store');
-Route::get('admin/report_submissions/{report_submissions}', 'Admin\reportSubmissionAPIController@show');
-Route::put('admin/report_submissions/{report_submissions}', 'Admin\reportSubmissionAPIController@update');
-Route::patch('admin/report_submissions/{report_submissions}', 'Admin\reportSubmissionAPIController@update');
-Route::delete('admin/report_submissions{report_submissions}', 'Admin\reportSubmissionAPIController@destroy');
--- routes/web.php
+++ routes/web.php
@@ -20,39 +20,12 @@ Route::namespace('Admin')->group(function () {
     Route::prefix('admin')->group(function () {
         Route::get('applications', ['as'=> 'admin.applications.index', 'uses' => 'applicationsController@index']);
 
-        Route::get('offresDeStages', ['as'=> 'admin.offresDeStages.index', 'uses' => 'offresDeStagesController@index']);
-        Route::post('offresDeStages', ['as'=> 'admin.offresDeStages.store', 'uses' => 'offresDeStagesController@store']);
-        Route::get('offresDeStages/create', ['as'=> 'admin.offresDeStages.create', 'uses' => 'offresDeStagesController@create']);
-        Route::put('offresDeStages/{offresDeStages}', ['as'=> 'admin.offresDeStages.update', 'uses' => 'offresDeStagesController@update']);
-        Route::patch('offresDeStages/{offresDeStages}', ['as'=> 'admin.offresDeStages.update', 'uses' => 'offresDeStagesController@update']);
-        Route::delete('offresDeStages/{offresDeStages}', ['as'=> 'admin.offresDeStages.destroy', 'uses' => 'offresDeStagesController@destroy']);
-        Route::get('offresDeStages/{offresDeStages}', ['as'=> 'admin.offresDeStages.show', 'uses' => 'offresDeStagesController@show']);
-        Route::get('offresDeStages/{offresDeStages}/edit', ['as'=> 'admin.offresDeStages.edit', 'uses' => 'offresDeStagesController@edit']);
-        Route::post('offresDeStages/{offresDeStages}/activate', ['as'=> 'admin.offresDeStages.activate', 'uses' => 'offresDeStagesController@activate']);
-    
         Route::resource('users', 'usersManager\UserController');
     
         Route::resource('roles', 'usersManager\RoleController');
     
         Route::resource('permissions', 'usersManager\PermissionController');
     
-        Route::get('reportSubmissions', ['as'=> 'admin.reportSubmissions.index', 'uses' => 'reportSubmissionController@index']);
-        Route::post('reportSubmissions', ['as'=> 'admin.reportSubmissions.store', 'uses' => 'reportSubmissionController@store']);
-        Route::get('reportSubmissions/create', ['as'=> 'admin.reportSubmissions.create', 'uses' => 'reportSubmissionController@create']);
-        Route::put('reportSubmissions/{reportSubmissions}', ['as'=> 'admin.reportSubmissions.update', 'uses' => 'reportSubmissionController@update']);
-        Route::patch('reportSubmissions/{reportSubmissions}', ['as'=> 'admin.reportSubmissions.update', 'uses' => 'reportSubmissionController@update']);
-        Route::delete('reportSubmissions/{reportSubmissions}', ['as'=> 'admin.reportSubmissions.destroy', 'uses' => 'reportSubmissionController@destroy']);
-        Route::get('reportSubmissions/{reportSubmissions}', ['as'=> 'admin.reportSubmissions.show', 'uses' => 'reportSubmissionController@show']);
-        Route::get('reportSubmissions/{reportSubmissions}/edit', ['as'=> 'admin.reportSubmissions.edit', 'uses' => 'reportSubmissionController@edit']);
-    
-        Route::get('internships', ['as'=> 'admin.internships.index', 'uses' => 'InternshipsController@index']);
-        Route::post('internships', ['as'=> 'admin.internships.store', 'uses' => 'InternshipsController@store']);
-        Route::get('internships/create', ['as'=> 'admin.internships.create', 'uses' => 'InternshipsController@create']);
-        Route::put('internships/{internships}', ['as'=> 'admin.internships.update', 'uses' => 'InternshipsController@update']);
-        Route::patch('internships/{internships}', ['as'=> 'admin.internships.update', 'uses' => 'InternshipsController@update']);
-        Route::delete('internships/{internships}', ['as'=> 'admin.internships.destroy', 'uses' => 'InternshipsController@destroy']);
-        Route::get('internships/{internships}', ['as'=> 'admin.internships.show', 'uses' => 'InternshipsController@show']);
-        Route::get('internships/{internships}/edit', ['as'=> 'admin.internships.edit', 'uses' => 'InternshipsController@edit']);
     });
 });
  
@@ -95,23 +68,7 @@ Route::middleware(['auth'])->group(function () {
         return view('welcome');
     });
 
-
-Route::get('offresDeStages', ['as'=> 'offresDeStages.create', 'uses' => 'offresDeStagesPFEController@create']);
-Route::post('offresDeStages', ['as'=> 'offresDeStages.store', 'uses' => 'offresDeStagesPFEController@store']);
-Route::get('offresDeStages/create', ['as'=> 'offresDeStages.create', 'uses' => 'offresDeStagesPFEController@create']);
-Route::get('offresDeStages/thanks', ['as'=> 'offresDeStages.thanks', 'uses' => 'offresDeStagesPFEController@thanks']);
-
 Auth::routes();
-
-
-
-
-
-Route::get('rapport', ['as'=> 'reportSubmissions.index', 'uses' => 'reportSubmissionController@index']);
-Route::post('reportSubmissions', ['as'=> 'reportSubmissions.store', 'uses' => 'reportSubmissionController@store']);
-Route::get('reportSubmissions/create', ['as'=> 'reportSubmissions.create', 'uses' => 'reportSubmissionController@create']);
-
-
 
 Route::namespace('Internship')->group(function () {
     // Controllers Within The "App\Http\Controllers\Admin" Namespace
@@ -137,8 +94,10 @@ Route::get('test2', function(){
 
     Route::namespace('Backend')->group(function () {
         Route::get('-/Dashboard', 'Dashboard');
-        Route::prefix('students')->group(function () {
-
+        Route::namespace('Internship')->group(function () {
+            Route::resource('internships', 'InternshipController');
+            Route::prefix('internships')->group(function () {
+            });
         });
     });
    /** ----------- New nomenclature for routes ------------- */
