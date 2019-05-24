79b64c47c1d9853337be5b18eafe9926e55ada9e
Meliani <melianiplus@gmail.com>
2019-05-24 06:04:59 +0100

    adding languages file

---
+++ resources/lang/fr/newlife.php
@@ -0,0 +1,8 @@
+<?php
+
+// resources/lang/en/messages.php
+
+return [
+    'update_my_data' => 'Mettre a jour mes donnees',
+    'welcome' => '"Nous accompagnons vos premiers pas vers l’entreprise"',
+];
