c086f4abff8f41c6a089e269e36a10793919d551
Meliani <melianiplus@gmail.com>
2019-05-24 18:58:24 +0100

    move delete all

--- resources/views/frontend/documents/partials/collections/old_docs.blade.php
+++ resources/views/frontend/documents/partials/collections/old_docs.blade.php
@@ -2,7 +2,7 @@
     <ul class="collection">
     <li class="collection-item avatar">
     <i class="material-icons circle blue">folder</i>
-    <span class="title">Autres versions de documents</span>
+    <span class="title">Versions precedentes des documents</span>
     <a href="#" class="secondary-content"><i class="material-icons">grade</i></a>
     @foreach ($documents as $doc)
     <p>
@@ -11,6 +11,8 @@
         </a>
     </p>
     @endforeach
-    </li>
+    </li><li class="collection-item avatar">
+    <a href={{ url('students/myDocuments?action[]=render&action[]=delete') }} class="waves-effect waves-light btn red">
+        <i class="material-icons right">delete</i>Supprimer tout</a></li>
     </ul>
     @endif
