6a9300c2a77a33416edef7aa73898f8d4214556e
Meliani <melianiplus@gmail.com>
2019-05-27 04:42:29 +0100

    Adding in progress to tease users before finishing the url management page

--- app/Http/Controllers/Core/UrlController.php
+++ app/Http/Controllers/Core/UrlController.php
@@ -12,6 +12,7 @@ class UrlController extends Controller
             /** decrypting function v1 */
         if($v=='v1'){
         $b = explode('/',decrypt($url));
+            return view('space.verification.in_progress');
             return view('space.verification.documents', compact('v','b'));
         }
         else{
---
+++ resources/views/space/verification/in_progress.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.ui.app')
+@section('title') | Digital documents @endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col s12 m12">
+        <div class="section">
+            <h3 class="header light center blue-text text-lighten-1">
+                Feature in progress, Stay tuned !
+            </h3>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
