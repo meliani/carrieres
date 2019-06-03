6c33e72de0aeb17195964e09c4d138c6b2f1a689
Meliani <melianiplus@gmail.com>
2019-06-03 02:45:34 +0100

    adding project and timslot model, controller

---
+++ app/Models/School/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\School;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /**
+     * hasMany internships
+     * hasMany professors
+     * hasMany students
+     * 
+     */
+}
---
+++ database/migrations/2019_05_29_173139_create_time_slots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('classroom_id')->unsigned()->nullable();
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->date('date')->nullable();
+            $table->integer('internship_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_slots');
+    }
+}
