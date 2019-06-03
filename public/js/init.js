15dedac911307c8ee8c5b5324282e7e0157a9083
Meliani <melianiplus@gmail.com>
2019-06-03 13:41:44 +0100

    timepicker personalization

--- public/js/init.js
+++ public/js/init.js
@@ -8,5 +8,14 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
   $('select').formSelect();
   $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
-  $('.timepicker').timepicker();
+  $('.timepicker').timepicker(
+    {
+      //default: 'now',
+      twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
+      donetext: 'OK',
+      format:"HH:ii:SS",
+      autoclose: false,
+      vibrate: true 
+    }
+  );
 });
