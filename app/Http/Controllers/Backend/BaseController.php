b4a0e595afcc0d8bf75dcb30bad4b9db5aab80a5
unknown <melianiplus@gmail.com>
2019-06-12 12:52:53 +0100

    making the things private again

--- app/Http/Controllers/Backend/BaseController.php
+++ app/Http/Controllers/Backend/BaseController.php
@@ -9,6 +9,6 @@ class BaseController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware(['auth','isAdmin']);        
+        $this->middleware(['auth','isAdmin']);        
     }
 }
