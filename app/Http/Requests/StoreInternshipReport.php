bc411602b8c538ed3f72acb811f1badee974904d
Mohamed EL MELIANI <melianiplus@gmail.com>
2019-09-25 16:39:30 +0100

    adding permissions requirements

--- app/Http/Requests/StoreInternshipReport.php
+++ app/Http/Requests/StoreInternshipReport.php
@@ -13,7 +13,7 @@ class StoreInternshipReport extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->user()->can('Use Frontend');
     }
 
     /**
