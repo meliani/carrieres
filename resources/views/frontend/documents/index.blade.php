e61265a0d25f95b11d995fb93a691bb3b124890c
Meliani <melianiplus@gmail.com>
2021-12-08 13:55:42 +0100

    automatisation of button appearing

--- resources/views/frontend/documents/index.blade.php
+++ resources/views/frontend/documents/index.blade.php
@@ -25,27 +25,11 @@
     @endif
     </li>
     @if(!auth()->user()->people->is_mobility)
-    <li class="collection-item avatar">
-        <i class="material-icons circle green">sync</i>
-    <a href={{ url('students/myDocuments?action[]=render&action[]=global_agreement') }} class="waves-effect waves-light btn">
-      <i class="material-icons right">save</i>Générer la convention Maroc et autres pays</a>
-    </li>
-    <li class="collection-item avatar">
-        <i class="material-icons circle green">sync</i>
-    <a href={{ url('students/myDocuments?action[]=render&action[]=france_agreement') }} class="waves-effect waves-light btn">
-      <i class="material-icons right">save</i>Générer la convention France</a>
-    </li>
+   @includeWhen(auth()->user()->people->internship->pays!='France','frontend.documents.partials.buttons.global')
+   @includeWhen(auth()->user()->people->internship->pays='France','frontend.documents.partials.buttons.france')
     @else
-    <li class="collection-item avatar">
-        <i class="material-icons circle green">sync</i>
-    <a href={{ url('students/myDocuments?action[]=render&action[]=mobility_global_agreement') }} class="waves-effect waves-light btn">
-      <i class="material-icons right">save</i>Générer la convention Mobilité autre pays que la france</a>
-    </li>
-    <li class="collection-item avatar">
-      <i class="material-icons circle green">sync</i>
-    <a href={{ url('students/myDocuments?action[]=render&action[]=mobility_france_agreement') }} class="waves-effect waves-light btn">
-      <i class="material-icons right">save</i>Générer la convention Mobilité en France</a>
-    </li>
+    @includeWhen(auth()->user()->people->internship->pays!='France','frontend.documents.partials.buttons.mobility')
+    @includeWhen(auth()->user()->people->internship->pays='France','frontend.documents.partials.buttons.mobility_france')
     @endif
     <li class="collection-item avatar">
         <i class="material-icons circle red">delete</i>
---
+++ resources/views/frontend/documents/partials/buttons/global.blade.php
@@ -0,0 +1,5 @@
+<li class="collection-item avatar">
+    <i class="material-icons circle green">sync</i>
+<a href={{ url('students/myDocuments?action[]=render&action[]=global_agreement') }} class="waves-effect waves-light btn">
+  <i class="material-icons right">save</i>Générer la convention Maroc et autres pays</a>
+</li>
