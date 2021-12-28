4aa057c08e5f38a401f8df17112233e1352b9794
Meliani <melianiplus@gmail.com>
2021-12-28 13:40:41 +0100

    some adjustments

--- resources/views/frontend/documents/pdf/templates/partials/signature_fields.blade.php
+++ resources/views/frontend/documents/pdf/templates/partials/signature_fields.blade.php
@@ -1,55 +1,72 @@
 <center>
-
     <table border="0" rules="all" class="tm61 tm102 tm103 tm104 tm105">
-     <tbody class="tm106">
-     <tr>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du Coordonnateur de fili&egrave;re</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du Chef d&#8217;Entreprise</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(Cachet de l&#8217;Entreprise)</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du stagiaire</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(Mention manuscrite</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&laquo;&nbsp;lu et approuv&eacute;&nbsp;&raquo;)</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Le Directeur de l&#8217;INPT</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(ou son Repr&eacute;sentant)</span></em></p>
-      </td>
-     </tr>
-     <tr>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom </span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du signataire</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom </span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du signataire</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom </span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du signataire</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em></p>
-      </td>
-      <td class="tm107 tm108 tm109">
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom </span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du signataire</span></em></p>
-      <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em></p>
-      </td>
-     </tr>
-     </tbody>
-    </table >
-    </center>
+        <tbody class="tm106">
+            <tr height=200px valign="top">
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du
+                                Coordonnateur de fili&egrave;re</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101">&#160;</span></em></p>
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du
+                                Chef d&#8217;Entreprise</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(Cachet de
+                                l&#8217;Entreprise)</span></em></p>
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Date et signature du
+                                stagiaire</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(Mention
+                                manuscrite</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">&laquo;&nbsp;lu et
+                                approuv&eacute;&nbsp;&raquo;)</span></em></p>
+
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Le Directeur de
+                                l&#8217;INPT</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">(ou son
+                                Repr&eacute;sentant)</span></em></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom
+                            </span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du
+                                signataire</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span
+                                class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em>
+                    </p>
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom
+                            </span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du
+                                signataire</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span
+                                class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em>
+                    </p>
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom
+                            </span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du
+                                signataire</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span
+                                class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em>
+                    </p>
+                </td>
+                <td class="tm107 tm108 tm109">
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">Nom et pr&eacute;nom
+                            </span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span class="tm89">du
+                                signataire</span></em></p>
+                    <p class="tm66 tm82 tm99"><em><span class="tm101"></span><span
+                                class="tm89">&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.</span></em>
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</center>
