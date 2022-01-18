82212d8e6d021e8172845fd0e87493b7ebe1f5b6
Meliani <melianiplus@gmail.com>
2022-01-18 18:09:13 +0100

    addind copy by click functionality

--- resources/views/backend/internships/partials/live_list.blade.php
+++ resources/views/backend/internships/partials/live_list.blade.php
@@ -1,4 +1,7 @@
 <div class="container col s12 m12">
+  <button wire:click="downloadExcel">Telecharger</button>
+  <div id="dateholder">hola</div>
+
   <table class="table highlight scale-transition scale-in">
     <thead>
       <tr>
@@ -16,25 +19,26 @@
     </thead>
     <tbody>
       @foreach ($internships as $internship)
+      {{-- @livewire('validate-intern',['internship' => $internship]) --}}
       <tr>
         @if(isset($internship->student))
         <td class="strong">{{ $internship->student->user_id }}
         </td>
-        <td><div
-          class="waves-effect sub strong">{{ $internship->student->full_name }}
+        <td  class="multiline center">
+          <div onclick="copyToClipboard('#full_name{{ $internship->student->user_id }}')" class="waves-effect sub strong"><p id="full_name{{ $internship->student->user_id }}">
+            {{ $internship->student->full_name }}</p>
           </div>
-          
-          
+
             <p><span class="new badge blue lighten-1 white-text" 
-            data-badge-caption="{{ $internship->student->phone }}"></p>
-            <p><span class="new badge orange lighten-3 white-text" 
-            data-badge-caption="{{ $internship->student->filiere_text }}"></p>
+            data-badge-caption="" onclick="copyToClipboard('#phone{{ $internship->student->user_id }}')" id="phone{{ $internship->student->user_id }}">{{ $internship->student->phone }}</p>
+            <p><span class="new badge orange lighten-1 white-text" 
+            data-badge-caption="" onclick="copyToClipboard('#filiere{{ $internship->student->user_id }}')" id="filiere{{ $internship->student->user_id }}">{{ $internship->student->filiere_text }}</p>
 
           @isset($internship->binome)
-          <span class="new badge orange lighten-3 white-text tooltipped" 
+          <span class="new badge purple lighten-3 white-text tooltipped" 
           data-delay="50" 
           data-tooltip="{{ $internship->binome->name }}"
-          data-badge-caption="Binome id : {{ $internship->binome->user_id }}">
+          data-badge-caption="">Binome id : {{ $internship->binome->user_id }}
           </span>
           @endisset
 
@@ -51,10 +55,10 @@
 
           </td>
           <td class="center">
-
+            {{-- <button class="waves-effect waves-light small-btn modal-trigger" href="#modal1" wire:click="$emit('ValidateIntern')" >live sign</button> --}}
           </td>
           <td class="multiline center">
-          @include('backend.internships.advising.jury',$internship->student)
+      
           </td>
           <td>
           @include('backend.internships.partials.actions')
@@ -63,7 +67,17 @@
           @endif
       </tr>
       @endif
+      {{-- @livewire('validate-intern-modal',['internship' => $internship], key('validate-modal-'.$internship->id)) --}}
+
       @endforeach
+       
     </tbody>
   </table>
 </div>
+
+
+
+@section('page-script')
+<script src="{{ asset('js/internTable.js') }}"></script>
+
+@endsection
