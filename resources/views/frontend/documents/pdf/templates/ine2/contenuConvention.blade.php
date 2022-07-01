adceff7782eed66daeddf324dfd1bd05300217c8
Meliani <melianiplus@gmail.com>
2022-07-01 13:33:28 +0100

    removing duplicate person's title next to name var

--- resources/views/frontend/documents/pdf/templates/ine2/contenuConvention.blade.php
+++ resources/views/frontend/documents/pdf/templates/ine2/contenuConvention.blade.php
@@ -107,7 +107,8 @@ types orient&eacute;s vers des objectifs globaux et sp&eacute;cifiques
     stagiaire d'autre part. </font></p>
 <p align="justify" style=" line-height: 100%"><font size="2" style="font-size: 11pt">Cette
 convention concerne l&rsquo;&eacute;l&egrave;ve ing&eacute;nieur :</font></p>
-<p align="justify" style=" line-height: 100%"><font size="2" style="font-size: 11pt"><b>{{ $internship->person->title }}</b></font><font size="2" style="font-size: 11pt"><b> {{ $internship->person->name }}</b>, &eacute;l&egrave;ve
+<p align="justify" style=" line-height: 100%">
+    <font size="2" style="font-size: 11pt"><b> {{ $internship->person->name }}</b>, &eacute;l&egrave;ve
 ing&eacute;nieur de la 2<sup>&egrave;me</sup>
 ann&eacute;e du cycle INE de l&rsquo;INPT.</font></p>
 <h3 class="western"><a name="_9f4lltgvdivs"></a>Article 2</h3>
