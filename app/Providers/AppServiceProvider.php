25f3f8286478379b5f10db0bcd624863821019c9
Meliani <melianiplus@gmail.com>
2022-11-08 02:06:44 +0100

    preventLazyLoading on prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setLocale(config('app.locale'));
 
+        // Laravel strict mode // wait until 9 upgrade
+        // Model::shouldBeStrict(! $this->app->isProduction());
+        
+    Model::preventLazyLoading(! $this->app->isProduction());
+    // Model::preventSilentlyDiscardingAttributes(); // throwing error
+    // Model::preventsAccessingMissingAttributes(); // throwing error
+
+
         Relation::morphMap([
             'internship' => 'App\Models\School\Internship',
         ]);
@@ -26,6 +37,17 @@ class AppServiceProvider extends ServiceProvider
             'warning' => 'alert-warning',
             'error' => 'alert-error',
         ]);
+
+/*          // trying to add a new valdiation rule
+            Validator::extend('valid_date_range', function ($attribute, $value, $parameters, $validator) {
+
+            $dateBeginning = New Carbon($parameters[0]); // do confirm the date format.
+        
+            $dateEnd = New Carbon($value);
+        
+            return $dateBeginning->diffInMonths($dateEnd) == $parameters[1];
+        }); */
+
     }
 
     /**
