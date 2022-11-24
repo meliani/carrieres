dd32c8a01746a5bb9f9c64372475f86bfd7b7343
Meliani <melianiplus@gmail.com>
2022-11-24 23:40:16 +0100

    adding parameter model and controller dor parameters table

---
+++ app/Models/Core/Parameter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parameter extends Model
+{
+    use HasFactory;
+}
