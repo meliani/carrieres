4f4dfd32b756e0387249ccb4b106c7306d698ca4
Meliani <melianiplus@gmail.com>
2022-11-27 01:22:15 +0100

    adding branche table and model associated to professor's model

---
+++ app/Http/Controllers/BrancheController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\School\Branche;
+use Illuminate\Http\Request;
+
+class BrancheController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\School\Branche  $branche
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Branche $branche)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\School\Branche  $branche
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Branche $branche)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\School\Branche  $branche
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Branche $branche)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\School\Branche  $branche
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Branche $branche)
+    {
+        //
+    }
+}
---
+++ app/Models/School/Branche.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\School;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Branche extends Model
+{
+    use HasFactory;
+}
