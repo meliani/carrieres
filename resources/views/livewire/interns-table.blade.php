cf8a863150ef6093e3d834a39fdff1f9c41a9c16
Meliani <melianiplus@gmail.com>
2022-11-27 01:33:37 +0100

    long time ago must be comitted

--- resources/views/livewire/interns-table.blade.php
+++ resources/views/livewire/interns-table.blade.php
@@ -1,15 +1,13 @@
-@section('css')
-    @livewireStyles
-@endsection
+
 
 <div>
     <input wire:model.debounce.300ms="search" type="text" placeholder="Rechercher des étudiants..."/>
 
     <ul>
         @include('backend.internships.partials.live_list')
+        {{-- to add for next upgrade fev 2021 --}}
+        {{-- @each('view.name', $jobs, 'job', 'view.empty') --}}
+
     </ul>
 </div>
 
-@section('scripts')
-    @livewireScripts
-@endsection
