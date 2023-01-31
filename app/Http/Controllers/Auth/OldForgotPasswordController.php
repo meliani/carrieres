985e6fe7d7874e155790833b1c6139a177465c27
mo <melianiplus@gmail.com>
2023-01-31 14:28:39 +0100

    saving old password controller

---
+++ app/Http/Controllers/Auth/OldForgotPasswordController.php
@@ -0,0 +1,173 @@
+<?php 
+  
+namespace App\Http\Controllers\Auth; 
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB; 
+use Carbon\Carbon; 
+use App\Models\User; 
+use Mail; 
+use Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
+
+  
+class OldForgotPasswordController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    /*********************************************************************** */
+    /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkResponse($request, $response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
+    }
+        /**
+     * Validate the email for the given request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+    }
+    
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        return Password::broker();
+    }
+    /**
+     * Get the needed authentication credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only('email');
+    }
+        /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return $request->wantsJson()
+                    ? new JsonResponse(['message' => trans($response)], 200)
+                    : back()->with('status', trans($response));
+    }
+/*************************************************************************************************** */
+      /**
+       * Write code on Method
+       *
+       * @return response()
+       */
+      public function showForgetPasswordForm()
+      {
+         return view('auth.forgetPassword');
+      }
+      /**
+       * Write code on Method
+       *
+       * @return response()
+       */
+      public function submitForgetPasswordForm(Request $request)
+      {
+          $request->validate([
+              'email' => 'required|email|exists:users',
+          ]);
+  
+          $token = Str::random(64);
+  
+          DB::table('password_resets')->insert([
+              'email' => $request->email, 
+              'token' => $token, 
+              'created_at' => Carbon::now()
+            ]);
+  
+          Mail::send('email.forgetPassword', ['token' => $token], function($message) use($request){
+              $message->to($request->email);
+              $message->subject('Reset Password');
+          });
+  
+          return back()->with('message', 'We have e-mailed your password reset link!');
+      }
+      /**
+       * Write code on Method
+       *
+       * @return response()
+       */
+      public function showResetPasswordForm($token) { 
+         return view('auth.forgetPasswordLink', ['token' => $token]);
+      }
+  
+      /**
+       * Write code on Method
+       *
+       * @return response()
+       */
+      public function submitResetPasswordForm(Request $request)
+      {
+          $request->validate([
+              'email' => 'required|email|exists:users',
+              'password' => 'required|string|min:6|confirmed',
+              'password_confirmation' => 'required'
+          ]);
+  
+          $updatePassword = DB::table('password_resets')
+                              ->where([
+                                'email' => $request->email, 
+                                'token' => $request->token
+                              ])
+                              ->first();
+  
+          if(!$updatePassword){
+              return back()->withInput()->with('error', 'Invalid token!');
+          }
+  
+          $user = User::where('email', $request->email)
+                      ->update(['password' => Hash::make($request->password)]);
+ 
+          DB::table('password_resets')->where(['email'=> $request->email])->delete();
+  
+          return redirect('/login')->with('message', 'Your password has been changed!');
+      }
+}
