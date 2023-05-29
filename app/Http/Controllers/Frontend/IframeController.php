4f8fbf5b916eda62ed39c0586ab1cd4ddfc9375f
mo <melianiplus@gmail.com>
2023-05-29 01:09:23 +0100

    changing iframe datasheet link

--- app/Http/Controllers/Frontend/IframeController.php
+++ app/Http/Controllers/Frontend/IframeController.php
@@ -15,7 +15,8 @@ class IframeController extends Controller
         $title = "Planning des soutenances ".config('school.current.academic_year');
         
         $iframe = '<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRtTUc6KeZ1YZ74AKHeiazI-r-axUry4HvOLB2GOhB6Od9tlaS2FUaTujt2iPbC6_iFVwkry9kW-j7_/pubhtml?gid=589152417&single=true&widget=false&headers=false&range=A:E" width="99%" height="1200px"></iframe>'; 
-        return view('frontend.iframe',compact('iframe','title'));
+        $iframe = '<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSYE_0hT4rrKdse3IzsHdQqxbeOnDh8-ePoFJXuPZgY3P9FEtxC6bXFuD3ePcP3COtiyBRqzkC6XT6z/pubhtml?gid=111104883&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="100%"></iframe>';
+            return view('frontend.iframe',compact('iframe','title'));
     }
     public function PlanningJeudis()
     {
