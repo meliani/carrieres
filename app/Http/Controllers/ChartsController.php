3f19ae2d8d3a2512c7f3d574b997164b4b31d050
mo <melianiplus@gmail.com>
2023-08-24 18:54:19 +0100

    url fix

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -12,10 +12,10 @@ class ChartsController extends Controller
         // Perform any necessary authentication or authorization checks here
 
         // Forward the request to the Superset instance
-        $url = 'https://localhost:8888/login'; // Replace with your Superset's HTTP URL
+        // $url = 'https://localhost:8888/'; // Replace with your Superset's HTTP URL
         // $response = Http::get($url . $request->getRequestUri());
 
-        // $url = 'https://192.168.1.8:8888/login'; // Replace with your Superset's HTTPS URL
+        $url = 'https://192.168.1.8:8888/'; // Replace with your Superset's HTTPS URL
         $response = Http::withOptions(['verify' => false])->get($url . $request->getRequestUri());
 
         return $response;
