4d9fd828ba958abe869e623081c869e9d023ed6b
mo <melianiplus@gmail.com>
2023-10-05 01:00:56 +0100

    login ui fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,41 +21,47 @@
                         <form method="POST" action="{{ route('login') }}">
                             {{ csrf_field() }}
                             <div class="row">
-                            {{ Form::textGroup([
-                                'name' => 'email',
-                                'value' ,
-                                'label' => __('Email adress'),
-                                'placeholder' ,
-                                'class' => 'materialize-textarea validate',
-                                'icon' => 'account_circle',
-                                'helper' ,
-                                'required' => 'required',
-                                'cols' => 6,
-                                'type' => 'email',
-                            ], $errors) }}
+                                {{ Form::textGroup(
+                                    [
+                                        'name' => 'email',
+                                        'value',
+                                        'label' => __('Email adress'),
+                                        'placeholder',
+                                        'class' => 'materialize-textarea validate',
+                                        'icon' => 'account_circle',
+                                        'helper',
+                                        'required' => 'required',
+                                        'cols' => 6,
+                                        'type' => 'email',
+                                    ],
+                                    $errors,
+                                ) }}
 
-                            {{ Form::textGroup([
-                                'name' => 'password',
-                                'value' ,
-                                'label' => __('Email adress'),
-                                'placeholder' ,
-                                'class' => 'materialize-textarea validate',
-                                'icon' => 'lock_outline',
-                                'helper' ,
-                                'required' => 'required',
-                                'cols' => 6,
-                                'type' => 'password',
-                            ], $errors) }}
+                                {{ Form::textGroup(
+                                    [
+                                        'name' => 'password',
+                                        'value',
+                                        'label' => __('Password'),
+                                        'placeholder',
+                                        'class' => 'materialize-textarea validate',
+                                        'icon' => 'lock_outline',
+                                        'helper',
+                                        'required' => 'required',
+                                        'cols' => 6,
+                                        'type' => 'password',
+                                    ],
+                                    $errors,
+                                ) }}
                             </div>
                             <div class="row">
-                                <div class="input-field col s4 m3">
+                                <div class="input-field col m2">
                                     <button type="submit" class="btn waves-effect waves-light">
-                                        Login
+                                        {{ __('Login') }}
                                     </button>
                                 </div>
-                                <div class="input-field col s4 m3">
+                                <div class="input-field col s4 m4">
                                     <a class="btn btn-link waves-effect waves-teal btn-flat"
-                                        href="{{ route('password.request') }}">Redéfinir ?</a>
+                                        href="{{ route('password.request') }}">{{ __('Reset password') }}</a>
                                 </div>
                             </div>
                         </form>
--- resources/views/components/forms/text.blade.php
+++ resources/views/components/forms/text.blade.php
@@ -1,35 +1,36 @@
 @php
-$type=array_get($params,'type','text');
-//dd($params);
-$class = array_get($params,'class');
+    $type = array_get($params, 'type', 'text');
+    //dd($params);
+    $class = array_get($params, 'class');
 @endphp
 
-@error(array_get($params,'name'))
-@php $class = array_get($params,'class').' invalid' @endphp
+@error(array_get($params, 'name'))
+    @php $class = array_get($params,'class').' invalid' @endphp
 @enderror
 
-<div class = "input-field {{ array_get($params,'inline') }} 
-    col 
-    s{{ array_get($params,'cols',6)*3 }}
-    m{{ array_get($params,'cols',6) }}
-    l{{ array_get($params,'cols',6)/1.5 }}
+<div
+    class="input-field {{ array_get($params, 'inline') }}
+    col
+    {{-- s{{ array_get($params,'cols',6)*3 }} --}}
+    m{{ array_get($params, 'cols', 6) }}
+    {{-- l{{ array_get($params,'cols',6)/1.5 }} --}}
     ">
-    <i class="material-icons blue-text prefix">{{ array_get($params,'icon') }}</i>
-    {!! Form::label(array_get($params,'name'), array_get($params,'label')) !!}
-    {{ Form::$type(array_get($params,'name'), array_get($params,'value'), 
-    ['class' => $class,
-     'placeholder' => array_get($params,'placeholder'),
-     'value' => array_get($params,'value'),
-     array_get($params,'required'),
-     array_get($params,'disabled')
+    <i class="material-icons blue-text prefix">{{ array_get($params, 'icon') }}</i>
+    {!! Form::label(array_get($params, 'name'), array_get($params, 'label')) !!}
+    {{ Form::$type(array_get($params, 'name'), array_get($params, 'value'), [
+        'class' => $class,
+        'placeholder' => array_get($params, 'placeholder'),
+        'value' => array_get($params, 'value'),
+        array_get($params, 'required'),
+        array_get($params, 'disabled'),
     ]) }}
 
-    <span class="helper-text" 
-    @error(array_get($params,'name'))
-    data-error="{{ $errors->first(array_get($params,'name')) }}"
+    <span class="helper-text"
+        @error(array_get($params, 'name'))
+    data-error="{{ $errors->first(array_get($params, 'name')) }}"
     @enderror
-    data-success="Parfait !">
-    {{ array_get($params,'helper') }}
+        data-success="">
+        {{ array_get($params, 'helper') }}
     </span>
 
 </div>
