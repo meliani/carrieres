7415bf1b8557f0f7e6b90909fa143b2cc4f3e9d1
mo <melianiplus@gmail.com>
2023-11-02 18:04:41 +0100

    patch account activation

--- resources/views/frontend/profile/person/cards/details.blade.php
+++ resources/views/frontend/profile/person/cards/details.blade.php
@@ -5,29 +5,31 @@
                 <div class="collapsible-header">
                     <h5 class='header5 blue-grey-text textlighten-5'>
                         <i class="small material-icons blue-grey-text textlighten-5">account_circle</i>
-                        Bonjour <b>{!! user()->student->full_name !!}</b>
+                        Bonjour <b>{!! user()->person->full_name !!}</b>
                     </h5>
                 </div>
                 <div class="collapsible-body ">
 
-                        <h6 class='header5 blue-grey-text textlighten-5'><p>Vous êtes en 
+                    <h6 class='header5 blue-grey-text textlighten-5'>
+                        <p>Vous êtes en
 
-                            <b>{{ trans_choice('labels.profile.years',user()->student->program_id) }}</b>, filière 
-                            <b>{{ user()->student->filiere_text }}</b>
-                            @if(user()->student->is_mobility)
+                            <b>{{ trans_choice('labels.profile.years', user()->person->program_id) }}</b>, filière
+                            <b>{{ user()->person->filiere_text }}</b>
+                            @if (user()->person->is_mobility)
                                 @lang('newlife.profile.mobility')
                             @endif
-                        
-                        </p></h6>
 
-                        <p class='header5 blue-grey-text'>
-                            @lang('newlife.profile.welcome')
                         </p>
+                    </h6>
+
+                    <p class='header5 blue-grey-text'>
+                        @lang('newlife.profile.welcome')
+                    </p>
 
                     </p>
                 </div>
             </li>
         </ul>
-        
+
     </div>
 </div>
