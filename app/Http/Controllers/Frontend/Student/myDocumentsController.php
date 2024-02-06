8134b3958f31acdd5ec12dc421985a60d7a16ad3
mo <melianiplus@gmail.com>
2024-02-06 12:37:33 +0100

    Refactor myDocumentsController.php

--- app/Http/Controllers/Frontend/Student/myDocumentsController.php
+++ app/Http/Controllers/Frontend/Student/myDocumentsController.php
@@ -9,22 +9,22 @@ use App\Http\Controllers\Core\Documents\renderController;
 use App\Models\Profile\Student;
 
 class myDocumentsController extends Controller
-    {
+{
 
     private $documents;
     private $student;
 
     public function __construct()
-        {
+    {
         $this->middleware(['auth', 'Student']);
-        }
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
-        {
+    {
 
         $this->student = user()->student;
         // dd($this->student);
@@ -33,48 +33,47 @@ class myDocumentsController extends Controller
             if (isset($request->action) && in_array('render', $request->action)) {
                 if (in_array('delete', $request->action)) {
                     user()->student->clearMediaCollection('internship');
-                    }
+                }
                 if (in_array('lr', $request->action)) {
                     $pdf = new renderController;
                     $pdf->recommendation_letter();
-                    }
-
                 }
+            }
             if (user()->student->hasMedia('internship')) {
                 $this->documents = user()->student->getMedia('internship');
-                }
+            }
             return view('frontend.documents.partials.fillforms', ['documents' => $this->documents]);
-            } else {
+        } else {
             if (isset($request->action)) {
-                if (user()->student->internship->is_valid == 0)
+                if (user()->student->internship->status === 'Draft' || !user()->student->internship->status)
                     return view('frontend.documents.partials.fillforms', ['documents' => $this->documents]);
 
                 if ($this->student->is_mobility == 1) {
                     //dd($this->person->is_mobility);
-                    }
+                }
                 if (in_array('delete', $request->action)) {
                     user()->student->clearMediaCollection('internship');
-                    }
+                }
                 if (in_array('render', $request->action)) {
                     $pdf = new renderController;
                     if (in_array('global_agreement', $request->action)) {
                         $pdf->conventionGlobale();
-                        }
+                    }
                     if (in_array('france_agreement', $request->action)) {
                         $pdf->conventionFrance();
-                        }
+                    }
                     if (in_array('mobility_global_agreement', $request->action)) {
                         $pdf->conventionMobilityAutre();
-                        }
+                    }
                     if (in_array('mobility_france_agreement', $request->action)) {
                         $pdf->conventionMobilityFrance();
-                        }
-                        if (in_array('lr', $request->action)) {
-                            $pdf = new renderController;
-                            $pdf->recommendation_letter();
-                            }
+                    }
+                    if (in_array('lr', $request->action)) {
+                        $pdf = new renderController;
+                        $pdf->recommendation_letter();
                     }
                 }
+            }
 
             if (user()->student->hasMedia('internship')) {
                 $this->documents = user()->student->getMedia('internship');
@@ -82,11 +81,10 @@ class myDocumentsController extends Controller
                     return view('frontend.documents.partials.fillforms');
 
                 return view('frontend.documents.index', ['documents' => $this->documents]);
-                } else
+            } else
                 return view('frontend.documents.index');
-            }
-
         }
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -94,9 +92,9 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-        {
+    {
         //
-        }
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -105,9 +103,9 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-        {
+    {
         //
-        }
+    }
 
     /**
      * Display the specified resource.
@@ -116,9 +114,9 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-        {
+    {
         //
-        }
+    }
 
     /**
      * Show the form for editing the specified resource.
@@ -127,9 +125,9 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
-        {
+    {
         //
-        }
+    }
 
     /**
      * Update the specified resource in storage.
@@ -139,9 +137,9 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-        {
+    {
         //
-        }
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -150,7 +148,7 @@ class myDocumentsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
-        {
+    {
         //
-        }
     }
+}
